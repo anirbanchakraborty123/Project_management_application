49faae36ce83b42a9efac7be525e3585eeb11fa9
anirbanchakraborty123 <anirbanchakraborty714@gmail.com>
2024-10-26 00:51:51 +0530

    updated router and urls file

--- management_api/router.py
+++ management_api/router.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ProjectViewSet, TaskViewSet, CommentViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
+# router.register(r'users', UserViewSet)
 router.register(r'projects', ProjectViewSet)
 router.register(r'tasks', TaskViewSet)
 router.register(r'comments', CommentViewSet)
--- management_api/urls.py
+++ management_api/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('user/register/', RegisterView.as_view(), name='register'),
-    path('user/login/', LoginView.as_view(), name='login'),
-    path('user/<int:id>/', UserDetailView.as_view(), name='user_detail'),
+    path('users/register/', RegisterView.as_view(), name='register'),
+    path('users/login/', LoginView.as_view(), name='login'),
+    path('users/<int:id>/', UserDetailView.as_view(), name='user_detail'),
 ]
